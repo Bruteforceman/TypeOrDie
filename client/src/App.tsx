3c73ff909040cc209875e5ae354e7074b3555c34
Raiyan Jamil <73040210+Raiyanx@users.noreply.github.com>
2022-08-07 20:17:54 +0600

    added event handler to window to take keyboard input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,12 @@ import './App.css';
 
 import Wordlist from './components/Wordlist';
 
+window.addEventListener('keydown', handlekey, true);
+
+function handlekey(e){
+  console.log(e);
+}
+
 function App() {
   return (
     <div className="App">
