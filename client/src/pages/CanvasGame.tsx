1f4c1605dcd29f7a546dbdb59941fa9baf17daa8
Bruteforceman <tasmeemreza@gmail.com>
2022-08-08 21:11:30 +0600

    refactored some code, canvas adds new words every 1.5 seconds now

--- client/src/pages/CanvasGame.tsx
+++ client/src/pages/CanvasGame.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import data from "../components/data";
 import "./CanvasGame.css";
 
 const height = 500; // Math.floor(window.innerHeight / 2);
@@ -8,6 +9,7 @@ class Player {
     posX : number;    
     height = 50;
     width = 50;
+    posY  = 30;
 
     constructor(posX : number) {
         this.posX = posX;
@@ -23,47 +25,107 @@ class Player {
     }
     draw(context : CanvasRenderingContext2D) {
         context.fillStyle = 'rgb(200, 0, 0)';
-        context.fillRect(this.posX, 30, this.width, this.height);
+        context.fillRect(this.posX, this.posY, this.width, this.height);
     }
 }
 
-let player : Player;
+class Enemy {
+    word   : string;
+    cur    : string;
+    posX   : number;
+    posY = height - 30;
 
-function initGame(context: CanvasRenderingContext2D) {
-    player = new Player(0); // creates a player in the corner
-    player.draw(context);
+    constructor(word : string, posX : number) {
+        this.cur  = word;
+        this.word = word;
+        this.posX = posX;
+    }
+    shoot(letter : string) {
+        if(this.cur[0].toLowerCase() === letter.toLowerCase()) {
+            this.cur = this.cur.slice(1);
+        }
+    }
+    move(dy : number) {
+        this.posY += dy;
+    }
+    draw(context : CanvasRenderingContext2D) {
+        context.fillStyle = 'rgb(0, 0, 0)';
+        context.font = '20px serif';
+        context.fillText(this.cur, this.posX, this.posY);
+    }
+    isDead(): boolean {
+        return this.posY < 0 || this.cur.length === 0;
+    }
 }
-function keyHandler(e: KeyboardEvent, context : CanvasRenderingContext2D) {
+
+function mutableFilter <T> (arr : T[], func: (el : T) => boolean): void {
+    const remaining : T[] = arr.filter(func);
+    const len = arr.length;
+    arr.splice(0, len);
+    for (const el of remaining) {
+        arr.push(el);
+    }
+}
+
+function addRandomEnemy(enemies: Enemy[]): void {
+    const idx = Math.floor(Math.random() * data.length); 
+    const posY = Math.floor(Math.random() * width);
+    enemies.push(new Enemy(data[idx], posY));
+}
+
+function initGame(context: CanvasRenderingContext2D) : () => void {
+    const player = new Player(0); // creates a player in the corner
+    const enemies = [new Enemy('lmfao', 30)];
     const playerSpeed = 14;
-    if(e.key === 'ArrowRight') {
-        player.move(playerSpeed);
-    } else if (e.key === 'ArrowLeft') {
-        player.move(-playerSpeed);
+    const enemySpeed = 1;
+
+    const keyHandler = (e: KeyboardEvent) => {    
+        if(e.key === 'ArrowRight') {
+            player.move(playerSpeed);
+        } else if (e.key === 'ArrowLeft') {
+            player.move(-playerSpeed);
+        } else {
+            for(const enemy of enemies) {
+                enemy.shoot(e.key);
+            }
+        }
     }
-    context.clearRect(0, 0, width, height);
-    player.draw(context);
+
+    const animate = () => {
+        context.clearRect(0, 0, width, height);
+        player.draw(context);
+        mutableFilter(enemies, (enemy) => !enemy.isDead());
+        for(const enemy of enemies) {
+            enemy.move(-enemySpeed);
+            enemy.draw(context);
+        }
+    }
+
+    const animationTimer = setInterval(animate, 50);
+    const enemyTimer = setInterval(() => addRandomEnemy(enemies), 1500);
+
+    window.addEventListener('keydown', keyHandler);
+
+    // must return a function that clears out the timer and the event listener
+    return () => {
+        window.removeEventListener('keydown', keyHandler);
+        clearInterval(animationTimer);
+        clearInterval(enemyTimer);
+    };
 }
+
+
 
 function CanvasGame() : JSX.Element {
     useEffect(() => {
         const canvas = document.getElementById('game') as HTMLCanvasElement;
-        let eventAdded = false;
-
-        let handleKeyEvent : (e : KeyboardEvent) => void;
         if(canvas !== null) {
             const context = canvas.getContext('2d');
-            if(context !== null) {
-                initGame(context);
-                handleKeyEvent = (e : KeyboardEvent) => keyHandler(e, context);
-                window.addEventListener('keydown', handleKeyEvent);
-                eventAdded = true;
+            if(context !== null) {                
+                return initGame(context);
             }
         }
-        return () => { 
-            if(eventAdded) {
-                window.removeEventListener('keydown', handleKeyEvent);
-            } 
-        };
+        return () => {};
     }, []);
     return (
         <div className="canvas-container">
