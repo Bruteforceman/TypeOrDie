ec9220868401277562c12e52bdcf89b80656585b
Bruteforceman <tasmeemreza@gmail.com>
2022-08-09 18:46:42 +0600

    added db connection

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,39 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { ConnectOptions, Mongoose } from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
+const mongoose = new Mongoose();
+
+
+// this should go into process.ENV
+const dbUser = process.env.DB_USER as string;
+const dbPassword = process.env.DB_PASSWORD as string;
+
+const mongoURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.cvhon.mongodb.net/?retryWrites=true&w=majority`;
+const dbName = 'TypeOrDie';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: dbName
+} as ConnectOptions).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(`Error connecting to MongoDB: ${err}`);
+});
+
+
+// note: the URL should start with http, not https
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.json({
+    'name': 'tasmeem reza',
+    'school': 'ndc college',
+  });
 });
 
 app.listen(port, () => {
